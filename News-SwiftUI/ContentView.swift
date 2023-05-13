5065351d7d39847cc186d79556d0f313c72c7d2e
Fiifi Pius <fiifipius@gmail.com>
2023-05-13 23:28:06 +0000

    Initial Commit

---
+++ News-SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  News-SwiftUI
+//
+//  Created by T.K Pius on 13/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ News-SwiftUI/News_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_SwiftUIApp.swift
+//  News-SwiftUI
+//
+//  Created by T.K Pius on 13/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct News_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
